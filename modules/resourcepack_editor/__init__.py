306ad1a992bb367aad3bf6cf450e53f482a54dc2
xXYxxdMC <xXYxxdMC@outlook.com>
2026-08-08 08:03:00 +0800

    Remove some reference

--- modules/resourcepack_editor/__init__.py
+++
@@ -1,7 +0,0 @@
-from .editor_backend import ResourcePackEditorBackend
-from .pack_analyzer import PackAnalyzer
-
-try:
-    from .git_handler import ResourcePackGit
-except ImportError:
-    ResourcePackGit = None
